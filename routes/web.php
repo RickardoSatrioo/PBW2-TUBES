fbe1ccb5ea370f145bd0eb431582c2ac38007ada
RickardoSatrioo <rickardosatrio@gmail.com>
2024-12-11 00:06:06 +0700

    tambah set up route dan view sesuai figma dan beberapa dependencied

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,16 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/home', function () {
+    return view('home');
+});
+Route::get('/after-auth', function () {
+    return view('after-auth');
+});
+Route::get('/landing', function () {
+    return view('landing');
+});
+
 
 Route::get('/dashboard', function () {
     return view('dashboard');
@@ -15,6 +25,24 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    Route::name('user.')->group(function() {
+        Route::get('/room/{id}', function () {
+            return view('room');
+        })->name('room');
+        Route::get('/reservation', function () {
+            return view('reservation');
+        })->name('reservation');
+        Route::get('/reservation', function () {
+            return view('history-reservation');
+        })->name('history_reservation');
+    });
+
+    Route::prefix('admin')->name('admin.')->group(function() {
+        Route::get('/verif-reservation', function () {
+            return view('room');
+        })->name('verif_reservation');
+    });
 });
 
 require __DIR__.'/auth.php';
